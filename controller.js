553031716f1e40afb8071a902705982ad6e312bb
Jasminy <jasminymatias4@gmail.com>
2023-09-26 11:39:35 -0300

    adiciona link para teste de expressão regular

---
+++ controller.js
@@ -0,0 +1,38 @@
+'use strict'; 
+
+const limparFormulario = (endereco) =>{
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+const preencherForumulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero); 
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+    
+    if(cepValido(cep.value)){
+        const dados = await fetch(url); 
+        const addres = await dados.json(); 
+        
+        if(addres.hasOwnProperty('erro')){ 
+            alert('CEP não encontrado!');
+        }else {
+            preencherForumulario(addres);
+        }
+    }else{
+        alert('CEP incorreto!');
+    } 
+}
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
